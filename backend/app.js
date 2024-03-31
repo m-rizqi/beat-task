573fe4ab1b263aaef7dc7c2fd17ea4a49165cae3
Muhammad Rizqi <mrizqi070502@gmail.com>
2024-03-31 11:11:50 +0700

    feat: user crud endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+
+connectDB();
+
+// Swagger definition
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Beat Task API',
+    version: '1.0.0',
+    description: 'Beat Task API Documentation',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000/api',
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
 
 // Middleware
 app.use(cors());
@@ -19,7 +48,9 @@ app.get('/', (req, res) => {
 });
 
 // Routes
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
