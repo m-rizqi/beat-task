87cb3c5e307ad6d42572742397a55f7b67fad61b
Perwira Akhdan Zumarsyah <perwiraakhdanz@gmail.com>
2024-03-31 23:46:54 +0700

    feat: create task and activity routes

---
+++ backend/routes/activityRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const activityController = require("../controllers/activityController");
+
+router.post("/", activityController.addActivity);
+router.put("/:id", activityController.updateActivity);
+router.delete("/:id", activityController.deleteActivity);
+router.get("/", activityController.getAllActivities);
+router.get("/:id", activityController.getActivity);
