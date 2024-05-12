ecdf1ef55484f190b453349e156c2dee53f46c81
hashifaq <hashifaq@gmail.com>
2024-05-12 19:33:47 +0700

    page music

--- frontend/app/music/page.js
+++ frontend/app/music/page.js
@@ -3,7 +3,7 @@
 import Navbar from "../components/navbar";
 import MusicCard from "../components/musicCard";
 
-export default function Home() {
+export default function Music() {
   return (
     <div className="bg-white">
       <Navbar></Navbar>
