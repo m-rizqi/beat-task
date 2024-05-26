c0f11708f3960a94379542e7312f3318b183a30f
Muhammad Rizqi <mrizqi070502@gmail.com>
2024-05-27 04:01:28 +0700

    add vercel conf

--- backend/config/config.js
+++ backend/config/config.js
@@ -10,5 +10,5 @@ module.exports = {
     HOST : process.env.HOST || 'localhost',
     PORT : process.env.PORT || 4000,
     JWT_SECRET: process.env.JWT_SECRET || 'beat-task',
-    DATABASE_HOST: process.env.DATABASE_HOST || '',
+    DATABASE_HOST: process.env.DATABASE_HOST || 'mongodb+srv://BeatTaskSenPro:aDqVlmWQE8KZIwCH@beat-task.c1wp0og.mongodb.net/?retryWrites=true&w=majority&appName=Beat-Task',
 }
